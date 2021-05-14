84052fe8e4afd26ba9a44fbd55e2bb04745f061d
Luis <luisabc132@gmail.com>
2021-05-14 18:24:20 +0200

    listo el hover siguiente paso el  desplegable

--- src/components/boardCard/BoardCard.js
+++ src/components/boardCard/BoardCard.js
@@ -1,10 +1,19 @@
 import './boardCard.css'
+import {useState} from 'react'
 const BoardCard= ({board}) => {
+    const [show, setShow] = useState(false);
     return(
-        <div className = "cardContainer">
+        <div className = "cardContainer" onMouseLeave = {()=>setShow(false)} onMouseEnter = {()=>setShow(true)}>
             <p className = "titleBoard">{board.name}</p>
             <p className = "numberPins">{board.numberOfpins} Pins</p>
-            <img className = "imgEdit" src = "https://riasdegalicia.com/wp-content/uploads/2021/05/Captura-de-pantalla-2021-05-11-a-las-23.41.50.png" />
+           {show ? (
+                <img className = "imgEdit" src = "https://riasdegalicia.com/wp-content/uploads/2021/05/Captura-de-pantalla-2021-05-11-a-las-23.41.50.png" />
+            ): console.log('nada')
+            }
+             
+          
+             
+           
             
         </div>
     )
